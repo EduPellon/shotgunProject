3c6909451515ef0f2aa90196a12894da395cb4bf
Nacho <0163473@up.edu.mx>
2017-06-11 01:59:08 -0500

    Completed

--- pipelineProject.py
+++ pipelineProject.py
@@ -45,8 +45,36 @@ option = raw_input("What do you want to upload?\n->Asset\n->Shot\n").lower()
 inputType = validateType(option)
 ID = raw_input("Type the ID of the %s:" %inputType) 
 goodID = validateID(ID)
-print validateIDShotgun(goodID)
 shotgunInfo = validateIDShotgun(goodID)
-
-print "Data correct"
+filters = [['entity','is', {'id': goodID, 'type': inputType}]]
+fields = ['id', 'code']
+requestVersions= sg.find("Version", filters, fields)
+print "Versions in %s:\n" %inputType
+for version in requestVersions:
+	print version['code']
+inputCode = raw_input("Set the name of the new Asset:\n")
+final_name = inputCode + ' v001'
+for version in requestVersions:
+	versionName = version["code"]
+	if inputCode.lower() in versionName.lower():
+		final_name = "%s%03d" %(versionName[:len(versionName)-3], int(versionName[len(versionName)-3:])+1)
+print "El nombre de tu archivo es: %s" %final_name
+description = raw_input("Escribe una descripcion de tu version:\n")
+data = {
+	'code': final_name,
+	'entity': {'id': goodID, 'type':inputType},
+	'description': description,
+	'sg_task': {'id': 2252 , 'type':'Task'},
+	'user': {'id':88, 'type': 'HumanUser'},
+	'sg_status_list': 'rev',
+	'project': {'id':110, 'type':'Project'}
+}
+result = sg.create("Version", data)
+correctPath = False
+while correctPath == False:
+	file_path = raw_input("Set the file path (include extension):\n")
+	if os.path.isfile(file_path) :
+		sg.upload("Version", result['id'], file_path, field_name="sg_uploaded_movie", display_name="Media")
+		correctPath = True
+print "All done, enjoy!"
 time.sleep(5)
